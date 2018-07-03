27144ddb08848e3dd5636a70512f317d4bdb2080
Dimitra Adam <adam@kleemann.gr>
2018-07-03 09:49:23 +0300

    Add project files.

---
+++ LittleThings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LittleThings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
